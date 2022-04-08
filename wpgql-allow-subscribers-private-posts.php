3d575efc36380073f0c7a1e33a1620038ea6253b
Cloudbop <boppincolin@gmail.com>
2022-04-09 00:12:18 +0100

    +bugfix: cdiff  +

--- wpgql-allow-subscribers-private-posts.php
+++ wpgql-allow-subscribers-private-posts.php
@@ -5,34 +5,32 @@
  * Version: 0.0.1-alpha
  * Author:
  * Author URI:
- * Description: This is an example plugin for extending WP-GraphQL wpgql-allow-subscribers-private-posts -remember! graphql_debug() and wp_send_json()
+ * Description: This is an example plugin for extending WP-GraphQL wpgql-allow-subscribers-private-posts -don't forget! graphql_debug(["customhookname"=>"invoked"]); and wp_send_json()
  */
-/** 
- * WP_QUERY sets an query argument of 'post_status' => 'public' for the underlying wpgraphql WP_User_Query, To adjust this, we can customise the `graphql_connection_query_args` WP_Query
- * eg: broadcast 'post_status'='private to non-authenticated requests.
+/**
+ * WP_QUERY sets an query argument of 'post_status' => 'public' for the underlying wpgraphql WP_User_Query, To adjust this, we can customise the `graphql_connection_query_args
  */
 add_filter( 'graphql_connection_query_args', function( $query_args, $connection_resolver ) {
   if ( // queries to target
     $connection_resolver instanceof \WPGraphQL\Data\Connection\PostObjectConnectionResolver ) {
-      if(//ensure the wp query is looking 4 private posts (the point of this hook) 
+      if( //ensure the wp query is querying post_status->private
         $query_args["graphql_args"]["where"]["status"]==="private" 
         // only logged in users
         && is_user_logged_in()
-        // update the WP_Query
         ){
+        // update the WP_Query args
         $query_args['post_status'] = ["private"];
     };
-  }
+  };
   return $query_args;
 }, 10, 2 );
 
-/** Filter the Post Model to make all private posts accessible to subscribers (and other logged in users.)
+/** Filter the Post Model to make all private posts queryable to subscribers (and other logged in users.)
  * WPGraphQL has a Model Layer that centralizes the logic to determine if any given object, or fields of the object, should be allowed to be seen by the user requesting data
  */
 add_filter( 'graphql_object_visibility', function( $visibility, $model_name, $data, $owner, $current_user ) {
-  // only apply our adjustments to the PostObject Model  
- if ( 'PostObject' === $model_name ) {
-    // $visibility = is_user_logged_in() ? 'public':$visibility;
+  // only apply our adjustments to the PostObject Model  beware! - more precise.
+  if ( 'PostObject' === $model_name ) {
     if(is_user_logged_in() && $data->post_status === "private" && $data->post_type==="post"){
       $visibility = 'public';
     }
@@ -40,37 +38,16 @@ add_filter( 'graphql_object_visibility', function( $visibility, $model_name, $da
   return $visibility;
 }, 10, 5 );
 
-//double check what is happening here.
-// error caused by adding argument to filter
-// , $resolver
-add_filter( 'graphql_connection_should_execute', function(
-  $should_execute
-){
-  // $current_user = wp_get_current_user();
-  $userLoggedIn = is_user_logged_in();
-  //  does it mean all logged in users will end calling this function on every bit of authenticated data (inc mutations) 
-  if($userLoggedIn){
-    return true; //$userLoggedIn || $should_execute;
-  } else {
-    return $should_execute;
-  }
-} );
-/* $resolver error == {
-{
-  "debugMessage": "Too few arguments to function {closure}(), 1 passed in /Users/colinrowntree/Local Sites/colinrtech20210705/app/public/wp-includes/class-wp-hook.php on line 309 and exactly 2 expected",
-  "message": "Internal server error",
-  "extensions": {
-      "category": "internal"
-  },
-  "locations": [
-      {
-          "line": 3,
-          "column": 10
-      }
-  ],
-  "path": [
-      "posts"
-  ],
-  "trace": [...]
-}
-}*/
+/** Overrule default if user is subscriber **/
+add_filter( 'graphql_connection_should_execute', function($should_execute, $resolver){
+  if ($resolver instanceof \WPGraphQL\Data\Connection\PostObjectConnectionResolver && is_user_logged_in()){
+    $current_user = wp_get_current_user();
+    // allow subscribers to execute reading 'private' posts 
+    if($current_user->caps['subscriber']){
+      return true;
+      // (beware! this could/would overule other post_type resolver behaviour where these conditionals are met)
+    }
+  } 
+  return $should_execute;
+  // bugfix - defaults ($priroity number, $args 1)  
+}, 10, 2);
