eba998a467ab7e8e07f540f7d2e8fffeca511585
Cloudbop <boppincolin@gmail.com>
2024-08-05 15:46:34 +0100

    +cleanup deprecation warnings +add extra visibilities for other post types

--- wpgql-allow-subscribers-private-posts.php
+++ wpgql-allow-subscribers-private-posts.php
@@ -14,15 +14,18 @@
 add_filter( 'graphql_connection_query_args', function( $query_args, $connection_resolver ) {
   if ( // queries to target
     $connection_resolver instanceof \WPGraphQL\Data\Connection\PostObjectConnectionResolver ) {
-      if( //ensure the wp query is querying post_status->private
-        $query_args["graphql_args"]["where"]["status"]==="private" 
-        // only logged in users, 
-        && is_user_logged_in()
-        // && ccould specify, wp_get_current_user()->caps['subscriber']
-        ){
-        // update the WP_Query args
-        $query_args['post_status'] = ["private"];
-    };
+    // conditionals that resolve to null get a deprecation warning
+    if(isset($query_args["graphql_args"]) && isset($query_args["graphql_args"]["where"]["status"])){
+        if( //ensure the wp query is querying post_status->private
+          $query_args["graphql_args"]["where"]["status"]==="private" 
+          // only logged in users, 
+          && is_user_logged_in()
+          // && ccould specify, wp_get_current_user()->caps['subscriber']
+          ){
+          // update the WP_Query args
+          $query_args['post_status'] = ["private"];
+      };
+    }
   };
   return $query_args;
 }, 10, 2 );
@@ -32,10 +35,18 @@ add_filter( 'graphql_connection_query_args', function( $query_args, $connection_
  * see - https://www.wpgraphql.com/2020/12/11/allowing-wpgraphql-to-show-unpublished-authors-in-user-queries/
  */
 add_filter( 'graphql_object_visibility', function( $visibility, $model_name, $data, $owner, $current_user ) {
-  // only apply our adjustments to the PostObject Model  beware! potential edgecases for CPT.
+  // only apply our adjustments to the PostObject Model  beware! potential edgecases for CPT. lost of model_name === PostObject
   if ( 'PostObject' === $model_name ) {
     if( $current_user->caps['subscriber'] && $data->post_status === "private" && $data->post_type==="post") {
       $visibility = 'public';
+    }
+    //
+    if(isset($current_user->caps['read_private_pages']) && $data->post_type==="page"){
+      $visibility = 'public';  
+    }
+    // 
+    if(isset($current_user->caps['read_private_portfolio']) && $data->post_type==="portfolio"){
+      $visibility = 'public';  
     }
   }
   return $visibility;
@@ -43,6 +54,7 @@ add_filter( 'graphql_object_visibility', function( $visibility, $model_name, $da
 
 /** Overrule default if user is subscriber **/
 add_filter( 'graphql_connection_should_execute', function($should_execute, $resolver){
+  //
   if ($resolver instanceof \WPGraphQL\Data\Connection\PostObjectConnectionResolver && is_user_logged_in()){
     $current_user = wp_get_current_user();
     // allow subscribers to execute reading 'private' posts
